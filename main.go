cc6777e7cfe02b2b4ee4320478b11a5087f008b6
Alef Martins <alef@edu.univali.br>
2018-08-19 17:42:58 -0300

    Remoção de conexoes entre os nodos

--- main.go
+++ main.go
@@ -65,7 +65,7 @@ func (node *Node) AppendConnection(newConnection *Node) {
 		node.connection_list.start = newConnection
 	} else {
 		currentNode := node.connection_list.start
-		for node.next_connection != nil {
+		for currentNode.next_connection != nil {
 			currentNode = currentNode.next_connection
 		}
 		currentNode.next_connection = newConnection
@@ -74,13 +74,14 @@ func (node *Node) AppendConnection(newConnection *Node) {
 }
 
 func (list *NodeList) RemoveNode(node *Node) *Node {
+	var previousNode *Node
+	previousNode = list.start
+	currentNode := list.start
+
 	if list.length == 0 {
 		fmt.Printf("Lista Vazia!\n")
 		return nil
 	}
-
-	var previousNode *Node
-	currentNode := list.start
 
 	//TODO: remoção por node ID
 	for currentNode.name != node.name {
@@ -93,11 +94,53 @@ func (list *NodeList) RemoveNode(node *Node) *Node {
 	}
 
 	previousNode.next = currentNode.next
+
+	if currentNode.connection_list.length > 0 {
+		currentConnected := currentNode.connection_list.start
+		for currentConnected != nil {
+			RemoveConnection(currentNode, currentConnected)
+			currentConnected = currentConnected.next_connection
+		}
+	}
+
 	currentNode.next = nil
 
 	list.length--
 
 	return currentNode
+}
+
+func (node *Node) RemoveNodeConnection(node_removed *Node) *Node {
+	if node.connection_list.length == 0 {
+		fmt.Printf("Lista Vazia!\n")
+		return nil
+	}
+
+	var previousNode *Node
+	previousNode = node.connection_list.start
+	currentNode := node.connection_list.start
+
+	//TODO: remoção por node ID
+	for currentNode.name != node_removed.name {
+		if currentNode.next_connection == nil {
+			fmt.Println("Nodo não encontrado %s %s", currentNode.name, node_removed.name)
+		}
+
+		previousNode = currentNode
+		currentNode = currentNode.next_connection
+	}
+
+	previousNode.next_connection = currentNode.next_connection
+	currentNode.next_connection = nil
+
+	node.connection_list.length--
+
+	return currentNode
+}
+
+func RemoveConnection(firstNode *Node, secondNode *Node) {
+	firstNode.RemoveNodeConnection(secondNode)
+	secondNode.RemoveNodeConnection(firstNode)
 }
 
 func makeConnection(firstNode *Node, secondNode *Node) {
@@ -134,6 +177,7 @@ func main() {
 	node_list.Append(&nodeD)
 
 	makeConnection(&nodeA, &nodeB)
+	makeConnection(&nodeA, &nodeC)
 	makeConnection(&nodeA, &nodeD)
 
 	printNodeList(*node_list)
@@ -146,6 +190,12 @@ func main() {
 	if nodo_removido != nil {
 		fmt.Printf("Nodo removido %s\n", nodo_removido.name)
 	}
+
+	fmt.Printf("Removendo Conexao\n")
+	time.Sleep(100 * time.Millisecond)
+
+	RemoveConnection(&nodeA, &nodeD)
+	printNodeList(*node_list)
 }
 
 /*func main() {
