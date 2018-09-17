60a2ee27448a18a2082eec0dbe92a5949ba25c38
Jucélio J. Silva <skalldihor@riseup.net>
2018-09-17 10:27:00 -0300

    edges don't has directions, for this work graph are not oriented.

--- edge.go
+++ edge.go
@@ -1,10 +1,22 @@
 package main
 
 type Edge struct {
-	// Unique ID
-	name   string
-	src    Node
-	dst    Node
+	name   string // unique id
+	begin  Node
+	end    Node
 	weight float64
 }
 
+
+func (e Edge) GetName() string {
+  return e.name
+}
+
+func (e Edge) GetWeight() float64 {
+  return e.weight
+}
+
+// return source and destination nodes.
+func (e Edge) GetNodes() (Node, Node) {
+  return e.begin, e.end
+}
