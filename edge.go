73bb5fe20d56a2561e24d6e786d832841b44272c
Jucélio J. Silva <skalldihor@riseup.net>
2018-09-17 08:00:53 -0300

    Use default creator method from struct edge.

--- edge.go
+++ edge.go
@@ -8,12 +8,3 @@ type Edge struct {
 	weight float64
 }
 
-// NewEdge returns a new Edge.
-func NewEdge(id string, src, dst Node, weight float64) *Edge {
-	return &Edge{
-		name:   id,
-		src:    src,
-		dst:    dst,
-		weight: weight,
-	}
-}
