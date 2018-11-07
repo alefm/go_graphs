ac340e5d12ea2a6b86d66b0a2737a50e21202c79
Jucélio Jair Silva <jjair.consulting@suntech.com.br>
2018-11-07 13:06:32 -0200

    ADD: endpoint para adicionar nodos e arestas

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"net/http"
 	"os/exec"
+	"strconv" 
 
 	"github.com/gorilla/mux"
 	"github.com/thedevsaddam/renderer"
 )
+
 
 var rnd *renderer.Render
 
@@ -37,19 +39,67 @@ func (graph *Graph) GetNodeByName(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Node{})
 }
 
+
+
+func (graph *Graph) CreateNode(w http.ResponseWriter, r *http.Request) {
+	node_name := r.FormValue("vertice_name")
+	node_weight := r.FormValue("vertice_weight")
+
+	tmp_node := Node{node_name, node_weight}
+	graph.AddNode(tmp_node)
+
+	graph.WriteToFile("output.dot")
+	cmd := exec.Command("dot", "-Tpng", "output.dot", "-o", "./static/graph.png")
+	cmd.Run()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (graph *Graph) CreateEdge(w http.ResponseWriter, r *http.Request) {
+	aresta_name := r.FormValue("aresta_name")
+	aresta_weight, err := strconv.ParseFloat(r.FormValue("aresta_weight"), 64)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	node_um := r.FormValue("vertice_um")
+	node_dois := r.FormValue("vertice_dois")
+	
+	first_node := graph.GetNode(node_um)
+	second_node := graph.GetNode(node_dois)
+
+	edge := Edge{aresta_name, *first_node, *second_node , aresta_weight, ""}
+
+	err = graph.AddEdge(edge)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	graph.WriteToFile("output.dot")
+	cmd := exec.Command("dot", "-Tpng", "output.dot", "-o", "./static/graph.png")
+	cmd.Run()
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
 func main() {
+	var graph = NewGraph()
 
-	graph := NewGraph()
 
+	/**
 	node1 := Node{"1", ""}
 	node2 := Node{"2", ""}
 	node3 := Node{"3", ""}
 	node4 := Node{"4", ""}
+	node5 := Node{"5", ""}
+	node6 := Node{"6", ""}
 
 	graph.AddNode(node1)
 	graph.AddNode(node2)
 	graph.AddNode(node3)
 	graph.AddNode(node4)
+	graph.AddNode(node5)
+	graph.AddNode(node6)
 
 	edge1 := Edge{"a", node2, node1, 4, ""}
 	edge2 := Edge{"b", node1, node3, -2, ""}
@@ -82,17 +132,22 @@ func main() {
 		fmt.Println(err5)
 	}
 
+	*/
 	graph.WriteToFile("output.dot")
 	cmd := exec.Command("dot", "-Tpng", "output.dot", "-o", "./static/graph.png")
 	cmd.Run()
 
+	/**
 	_, predecessor := graph.FloydAlgorithm()
 	path := graph.FloydPath(predecessor, node1.Name, node4.Name)
 
 	fmt.Println(path)
+	*/
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", GetGraph)
+	router.HandleFunc("/graph/nodes/", graph.CreateNode).Methods("POST")
+	router.HandleFunc("/graph/edges/", graph.CreateEdge).Methods("POST")
 	router.HandleFunc("/graph/nodes/{name}", graph.GetNodeByName).Methods("GET")
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
